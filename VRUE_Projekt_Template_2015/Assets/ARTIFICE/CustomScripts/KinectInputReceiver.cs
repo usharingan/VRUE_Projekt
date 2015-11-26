8c897afe09399089628becbd0dfdebe4a1457c37
Oana Moraru <tsunade_oana@yahoo.com>
2015-11-26 16:27:26 +0100

    Added new Kinect script

---
+++ VRUE_Projekt_Template_2015/Assets/ARTIFICE/CustomScripts/KinectInputReceiver.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KinectInputReceiver : MonoBehaviour {
+
+	// TODO
+	// 1) hol dir alle instrumente
+	// 2) Instrumente selektiert/beruehrt -> bool variable getoggled
+	// 3) fetch alle input tasten, die fuer den Kinect in Faast gut funktionieren
+	// 4) bring damit selektierte instrumente zum singen - audio start...
+	// 5) wenn lange nix mehr - instrum werden leiser -> audio leiser
+	// 6) nochmal instrument beruehren/"selektieren" -> bool unselected (kann getauscht werden etc), audio stop
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
